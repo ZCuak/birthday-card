84c316ba008e212059aea08194cd498cb3a653fd
ZCuak <gejomial@gmail.com>
2024-09-21 12:50:46 -0500

    Regalo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BirthdayCardComponent } from './birthday-card/birthday-card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BirthdayCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/birthday-card/birthday-card.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-birthday-card',
+  templateUrl: './birthday-card.component.html',
+  styleUrls: ['./birthday-card.component.css']
+})
+export class BirthdayCardComponent {
+  showVoucher = false; // Controla el cambio entre las cartas
+
+  // Alternar entre las cartas al hacer clic
+  toggleCard() {
+    this.showVoucher = !this.showVoucher; // Cambia el estado entre la primera y segunda carta
+    this.playSound();
+    if (this.showVoucher) {
+      this.launchFireworks(); // Lanza los fuegos artificiales cuando aparece la segunda carta
+    }
+  }
+
+
+
+  // Función para reproducir sonido
+  playSound() {
+    const audio = new Audio('../sound.mp3'); // Ruta del archivo de sonido
+    audio.play();
+  }
+
+  // Función para lanzar los fuegos artificiales
+  launchFireworks() {
+    const duration = 5 * 1000; // Duración de 5 segundos
+    const animationEnd = Date.now() + duration;
+    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    function randomInRange(min: number, max: number) {
+      return Math.random() * (max - min) + min;
+    }
+
+    const interval = setInterval(() => {
+      const timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      const particleCount = 50 * (timeLeft / duration);
+
+      // Disparar confetti desde dos orígenes
+      (window as any).confetti(Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 }
+      }));
+      (window as any).confetti(Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 }
+      }));
+    }, 250);
+  }
+}
